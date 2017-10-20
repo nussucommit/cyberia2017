5835593da399017028f3ab8132a99dcef974dd02
Guek Ling <yeoyueling@gmail.com>
2017-10-20 11:12:57 +0800

    modified main page: added flipclock

--- HTML/js/components/flipclock-time.js
+++ HTML/js/components/flipclock-time.js
@@ -1,20 +1,17 @@
-var clock;
-
 $(document).ready(function() {
-
 	// Grab the current date
 	var currentDate = new Date();
 
 	// Set the date of start
-	var futureDate  = new Date(2017, 15, 9, 23, 59);
+	var futureDate  = new Date(2017, 12, 13, 00, 00, 00, 00);
 
 	// Calculate the difference in seconds between the future and current date
 	var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 	diff = diff > 0 ? diff : 0;
 
 	// Instantiate a coutdown FlipClock
-	clock = $('.clock').FlipClock(diff, {
+	var clock = $('.clock').FlipClock(diff, {
 		clockFace: 'DailyCounter',
-		countdown: true
+		countdown: true,
 	});
 });
