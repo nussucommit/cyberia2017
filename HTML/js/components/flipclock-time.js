3ca8a15051b6813f6a620d2a4dcc4dd8e6340ee2
Guek Ling <yeoyueling@gmail.com>
2017-10-24 15:31:10 +0800

    fixed countdown

--- HTML/js/components/flipclock-time.js
+++ HTML/js/components/flipclock-time.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	var currentDate = new Date();
 
 	// Set the date of start
-	var futureDate  = new Date(2017, 12, 13, 00, 00, 00, 00);
+	var futureDate  = new Date(2017, 11, 13);
 
 	// Calculate the difference in seconds between the future and current date
 	var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
